bc784a8642ef2620554a1e85b56d5459ee43c3fa
nds95 <nds95@kakao.com>
2021-01-29 18:22:16 +0900

    Add AverageFinder.java

--- src/AverageFinder.java
+++ src/AverageFinder.java
@@ -1,5 +1,11 @@
 public class AverageFinder {
     double computeAverage(int[] intArray) {
-        int
-    }
+	    int sum = 0;
+	    
+	    for (int num : intArray) {
+		   sum += num;
+	    }
+
+	    return (double) sum / intArray.length;
+	}
 }

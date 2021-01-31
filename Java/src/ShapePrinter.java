91f08507a7b7c2e21eeedf54416c6b8824c82ed2
nds95 <nds95@kakao.com>
2021-01-31 23:40:36 +0900

    fixed ShapePrinter.java

--- Java/src/ShapePrinter.java
+++ Java/src/ShapePrinter.java
@@ -19,4 +19,27 @@ public class ShapePrinter {
 						System.out.println(pyramid);
 				}
 		}
+
+		public void printFloydsPyramid(int height) {
+				int a = 1;
+				int b = 1;
+				int h_Result = height * (height + 1) / 2;
+				String h_String = Integer.toString(h_Result);
+				String a_String = Integer.toString(a);
+				for (int i = 1; i <= height; i++) {
+						String blank = "";
+						for (int j = a; a_String.length() <= h_String.length(); j++) {
+								blank += " ";
+						}
+						while (a <= b) {
+								if (a == b) {
+										System.out.println(blank + a);
+								} else {
+										System.out.print(blank + a);
+								}
+								a += 1;
+						}
+				b += (i + 1);
+				}
+		}
 }

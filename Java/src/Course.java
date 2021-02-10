22377920a92e2f8df092ac7a55a73c7b6196bfa4
nds95 <nds95@kakao.com>
2021-02-10 23:09:12 +0900

    fixed GradeCalculator.java

--- Java/src/Course.java
+++ Java/src/Course.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-
 public class Course{
-	public final int CREDIT_MAJOR = 3;
-	public final int CREDIT_GENERAL = 2;
+	public static final int CREDIT_MAJOR = 3;
+	public static final int CREDIT_GENERAL = 2;
 	
-	public ArrayList<String> lecture;
-	public ArrayList<String> score;
-	public ArrayList<integer> credit;
+	public final String name;
+	public final String grade;
+	public final int credit;
 
-	public Course(String lecture, int credit, String score) {
-		this.lecture.add(lecture);
-		this.score.add(score);
-		this.credit.add(credit);
+	public Course(String name, int credit, String grade) {
+		this.name = name;
+		this.grade = grade;
+		this.credit = credit;
 	}
 }
--- Java/src/GradeCalculator.java
+++ Java/src/GradeCalculator.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
+
 public class GradeCalculator {
-	String name;
-	int score;
-	int credit;
+	public final String name;
+	private double currentScore;
+	private int currentCredit;
+
+	private final ArrayList<Course> courses = new ArrayList<> ();
+
+	public GradeCalculator(String name, double currentScore, int currentCredit) {
+		this.name = name;
+		this.currentScore = currentScore;
+		this.currentCredit = currentCredit;
+	}
 
 	public void addCourse(Course course) {
+		courses.add(course);
+	}
+
+	public void print() {
+		System.out.println("직전 학기 성적: " + currentScore + " (총 " + currentCredit + "학점)");
 		
-		
+		double scoreSum = 0;
+		int creditSum = 0;
+		for (Course c : courses) {
+			scoreSum += c.getScore() * c.credit;
+			creditSum += c.credit;
+		}
 
-
-
+		System.out.println("이번 학기 성적: " + (scoreSum / creditSum) + " (총 " + creditSum + "학점)");
+	}
+}

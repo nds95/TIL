be39496cf1a10fc258947aafa4934009b757a96a
nds95 <nds95@kakao.com>
2021-02-25 22:07:17 +0900

    node.js study file

--- Nodejs/study/main.js
+++ Nodejs/study/main.js
@@ -1,16 +1,42 @@
-const fs = require('fs');
+const http = require('http');
+const express = require('express');
+const { request, response } = require('express');
 
-/* console.log('start');
+const app = express();
 
-let content = fs.readFileSync('./new', 'utf-8');
-console.log(content);
+const users = ['DS', 'JJ', 'JS', 'CH'];
 
-console.log('Finish'); */
+// const server = http.createServer((request, response) => {
+//   if (request.url === '/') {
+//     response.end('<h1>Welcome!</h1>');
+//  } else if (request.url === '/users') {
+//     response.end('<h1>${users}</h1>');
+//  } else if (request.url.split('/')[1] === 'users') {
+//    const userIdx = request.url.split('/')[2];
+//    const userName = users[userIdx - 1];
 
-console.log('start');
+//    response.end('<h1>${usersName}</h1>');
+//  } else {
+//     response.end('<h1>Page Not Available</h1>');
+//  }
+// });
+// server.listen(3000);
 
-fs.readFile('./new', 'utf8', (error, data) => {
-  console.log(data);
+app.get('/', (request, response) => {
+  response.end('<h1>Welcome!</h1>');
 });
 
-console.log('Finish');
+app.get('/users', (request, response) => {
+  response.end('<h1>${users}</h1>');
+});
+
+app.get('/users/:id', (request, response) => {
+  const userName = users[request.params.id - 1];
+  response.end('<h1>${userName}</h1>');
+});
+
+app.get('*', (request, response) => {
+  response.end('<h1>Page Not Available</h1>');
+});
+
+app.listen(3000);

6431842ca4105467e6db688232977baf3cc447fd
nds95 <nds95@kakao.com>
2021-02-26 22:03:14 +0900

    node.js study

--- Nodejs/study/main.js
+++ Nodejs/study/main.js
@@ -1,42 +1,39 @@
-const http = require('http');
-const express = require('express');
-const { request, response } = require('express');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-const app = express();
-
-const users = ['DS', 'JJ', 'JS', 'CH'];
-
-// const server = http.createServer((request, response) => {
-//   if (request.url === '/') {
-//     response.end('<h1>Welcome!</h1>');
-//  } else if (request.url === '/users') {
-//     response.end('<h1>${users}</h1>');
-//  } else if (request.url.split('/')[1] === 'users') {
-//    const userIdx = request.url.split('/')[2];
-//    const userName = users[userIdx - 1];
-
-//    response.end('<h1>${usersName}</h1>');
-//  } else {
-//     response.end('<h1>Page Not Available</h1>');
-//  }
-// });
-// server.listen(3000);
-
-app.get('/', (request, response) => {
-  response.end('<h1>Welcome!</h1>');
+var app = http.createServer(function(request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  if(_url == '/') {
+    _url = '/index.html';
+  }
+  if(_url == '/favicon.ico') {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  var template = `
+  <!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${queryData.id}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.html">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=Javascript">JavaScript</a></li>
+  </ol>
+  <h2>${queryData.id}</h2>
+  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+  <img src="coding.jpg" width="100%">
+  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+  </p>
+</body>
+</html>
+  `
+  response.end(template);
 });
-
-app.get('/users', (request, response) => {
-  response.end('<h1>${users}</h1>');
-});
-
-app.get('/users/:id', (request, response) => {
-  const userName = users[request.params.id - 1];
-  response.end('<h1>${userName}</h1>');
-});
-
-app.get('*', (request, response) => {
-  response.end('<h1>Page Not Available</h1>');
-});
-
 app.listen(3000);

7eb5d94d501bb0ed43e6dda98661bdb38972001d
nds95 <nds95@kakao.com>
2021-04-06 23:34:41 +0900

    fix

--- webscraping_basic/6_bs4.py
+++ webscraping_basic/6_bs4.py
@@ -12,7 +12,7 @@ soup = BeautifulSoup(res.text, "lxml")
 print(soup.a.attrs) # a태그의 속성정보를 객체형식으로 반환
 print(soup.a["href"]) # a태그의 href 속성값을 반환한다.
 
-print(soup.find("a", attrs={"class":"Nbtn_upload"}))
+print(soup.find("a", attrs={"class":"Nbtn_upload"}));
 # next_sibling > 다음 형제 element 찾기
 # previous_sibling > 이전 형제 element 찾기
   

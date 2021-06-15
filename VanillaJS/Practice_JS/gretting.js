669112adebb22a6fa2be26253de7a8fcf38166d8
nds95 <nds95@kakao.com>
2021-06-15 22:47:16 +0900

    Algorithm

--- VanillaJS/Practice_JS/gretting.js
+++ VanillaJS/Practice_JS/gretting.js
@@ -1,8 +1,6 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings"),
-  clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+  greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
@@ -27,13 +25,14 @@ function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
 
-  const date = new date();
-  const hours = date.getHours();
-  if(hours < 12){
-    greeting.innerText = `Good Morning ${text}`;
-  } else {
-    greeting.innerText = `Good evening ${text}`;
-  }
+  //const date = new Date();
+  //const hours = date.getHours();
+  //if(hours < 12){
+    //greeting.innerText = `Good Morning ${text}`
+  //} else {
+    //greeting.innerText = `Good evening ${text}`
+  //}
+  greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){

42355fc4018157aed1c5d46bbf8a7937bf6d8199
nds95 <nds95@kakao.com>
2021-11-19 09:11:39 +0900

    navbar update

--- web_Publishing/discord_v2/main.js
+++ web_Publishing/discord_v2/main.js
@@ -1,15 +1,45 @@
 'use strict';
 
-var navbar = document.querySelector(`#main-header`);
+//global variable
+
+//navbar opacity control
+
+var navbar = document.querySelector(`.opacity`);
 var navbarHeight= navbar.getBoundingClientRect().height;
-var createOpacity = document.createAttribute('display');
-createOpacity.value = "0.1";
+
+function calcHeight(height) {
+    var result = parseFloat(height / 90).toFixed(4);
+    return result;
+};
 
 document.addEventListener("scroll", () => {
-    console.log(`navbar : ` + navbarHeight);
     if(window.scrollY > navbarHeight) {
-        navbar.style.opacity = "0.1";
-    } else {
         navbar.style.opacity = "1";
+    } else {
+        navbar.style.opacity = calcHeight(window.scrollY).toString();
     }
 }, true);
+//navbar search input display controll
+
+var navSearchInput = document.querySelector('.header__search > input');
+var searchInput = document.querySelector('.search--input');
+
+document.addEventListener("scroll", () => {
+    if(window.scrollY > 179) {
+        navSearchInput.classList.add('appear');
+        navSearchInput.classList.remove('disappear');
+    } else {
+        navSearchInput.classList.remove('appear');
+        navSearchInput.classList.add('disappear');
+    }
+}, true);
+
+document.addEventListener("scroll", () => {
+    if(window.scrollY > 179) {
+        searchInput.classList.add('disappear');
+        searchInput.classList.remove('appear');
+    } else {
+        searchInput.classList.remove('disappear');
+        searchInput.classList.add('appear');
+    }
+})

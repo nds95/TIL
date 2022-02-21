2eae333b07011c706555d37efab02fcce9a2fcb1
nds95 <nds95@kakao.com>
2022-02-22 01:01:37 +0900

    1110 complete

--- Algorithm/1110.js
+++ Algorithm/1110.js
@@ -1,26 +1,28 @@
-let input = require('fs').readFileSync('./example.txt').toString().trim();
+let input = Number(require('fs').readFileSync('./example.txt').toString().trim());
 if(input >= 10) {
-  input = input.split("").map(value => +value);
+  input = input.toString().split("").map(value => +value);
+} else {
+  input = (input * 10).toString().split("").map(value => +value);
 }
 let result = 0;
 let [a, b] = input;
 let count = 0;
 
-while(count < 6) {
+while(true) {
   count ++;
-  console.log(`a = ${a}, b = ${b}`);
-  let newNum = a + b;
-  if(newNum >= 10) {
-    newNum = a + b - 10;
-    console.log(`newNum = ${newNum}`);
-  }
-  result = (b * 10) + newNum;
-  if(result === input[0] * 10 + input[1]){
+  if(b === undefined) {
     break;
   }
-  console.log(`result = ${result}`);
-  a = Number(result.toString()[0]);
-  b = Number(result.toString()[1]);
+  let newNum = a + b;
+  a = b;
+  if(newNum >= 10){
+    b = newNum - 10;
+  } else {
+    b = newNum;
+  }
+  if(a.toString() + b.toString() === input[0].toString() + input[1].toString()) {
+    break;
+  }
 }
 
 console.log(count);
